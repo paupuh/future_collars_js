ad5462f8d6e6a17f06de87f456f7aafed2024f5e
paupuh <116916759+paupuh@users.noreply.github.com>
2025-01-22 08:34:49 +0100

    exercisse done

---
+++ constants_number.js
@@ -0,0 +1,11 @@
+const firstNumber = 10;
+const secondNumber = 5;
+const result = (firstNumber + secondNumber) / 2;
+
+
+
+
+// Zadeklaruj i zainicjalizuj stałe firstNumber i secondNumber dowolnymi wartościami liczbowymi.
+// Następnie stwórz stałą result, która będzie przechowywała sumę dwóch poprzednich stałych, 
+// podzieloną przez 2. Pamiętaj o tym, 
+// że podobnie jak w matematyce, w JS musisz dbać o kolejność wykonywania działań i potrzebne będą nawiasy (). Powodzenia!
